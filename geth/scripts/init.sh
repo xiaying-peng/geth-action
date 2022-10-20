2e88aa975aaef30687fda1fc26603842c68b769c
Xiaying Peng <xiaying.peng@coinbase.com>
2022-10-20 00:03:35 -0700

    improve geth action

---
+++ geth/scripts/init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -eu
+
+geth init /root/config/genesis.json
+geth --password /root/config/password.txt account import /root/config/private-key1.txt
+geth --password /root/config/password.txt account import /root/config/private-key2.txt 
+
