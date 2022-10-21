c3f1a1abd26ce306cf507e08daffa0b5e551746f
Xiaying Peng <xiaying.peng@coinbase.com>
2022-10-21 09:49:24 -0700

    add 1 more account

--- geth/scripts/init.sh
+++ geth/scripts/init.sh
@@ -4,5 +4,7 @@ set -eu
 
 geth init /root/config/genesis.json
 geth --password /root/config/password.txt account import /root/config/private-key1.txt
-geth --password /root/config/password.txt account import /root/config/private-key2.txt 
+geth --password /root/config/password.txt account import /root/config/private-key2.txt
+geth --password /root/config/password.txt account import /root/config/private-key3.txt 
+
 
